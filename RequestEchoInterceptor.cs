7e84419a9d444666c0aec4a7051c860e2046547e
Theo Danzfuss <theodanzfuss@gmail.com>
2018-09-20 08:51:49 +0200

    Add echo on GET

---
+++ RequestEchoInterceptor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Interceptor
+{
+    public class RequestEchoInterceptor : IHttpHandler
+    {
+        public bool IsReusable
+        {
+            get { return false; }
+        }   
+
+        public void ProcessRequest(HttpContext context)
+        {
+            if (context.Request.Headers != null && context.Request.Headers.Count > 0)
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (string key in context.Request.Headers.AllKeys)
+                {
+                    sb.Append(key).Append(" : ").Append(context.Request.Headers[key]).AppendLine();
+                }
+                context.Response.Clear();
+                context.Response.Write(sb.ToString());
+                context.Response.Flush();
+            }
+        }
+    }
+}
