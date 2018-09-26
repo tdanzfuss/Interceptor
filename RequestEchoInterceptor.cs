d46ad37c252c6932c59804c586df2b85f7a25ae2
Theo Danzfuss <theodanzfuss@gmail.com>
2018-09-26 10:05:15 +0200

    Add GET Handler for injecting JWT

--- RequestEchoInterceptor.cs
+++ RequestEchoInterceptor.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
+using Newtonsoft.Json.Linq;
 
 namespace Interceptor
 {
@@ -16,7 +17,24 @@ namespace Interceptor
 
         public void ProcessRequest(HttpContext context)
         {
-            if (context.Request.Headers != null && context.Request.Headers.Count > 0)
+            string authToken = context.Request.Headers.Get("Authorization"); // JWT must be injected by Webseal
+            if ((authToken == null) || (authToken.Length <= 0))
+            {
+                authToken = "Im not a  JWT...";
+            }
+
+            dynamic json = JObject.Parse("{}");
+            json.jwt = authToken;
+            //if ((authToken != null) && (authToken.Length > 0))
+            //{            
+                context.Response.Clear();
+                context.Response.Write(context.Request.Path + " <b>REDIRECT WITH JWT: </b>" + authToken);
+                context.Response.Write(string.Format(generateScriptTags(),
+                    setLocalStorage("actionType", ((JObject)json).ToString (Newtonsoft.Json.Formatting.None)),
+                    getRedirectionUrl(context.Request.Url.AbsolutePath)));
+                context.Response.Flush();
+            //}
+            /*if (context.Request.Headers != null && context.Request.Headers.Count > 0)
             {
                 StringBuilder sb = new StringBuilder();
                 foreach (string key in context.Request.Headers.AllKeys)
@@ -26,7 +44,28 @@ namespace Interceptor
                 context.Response.Clear();
                 context.Response.Write(sb.ToString());
                 context.Response.Flush();
-            }
+            }*/
+        }  
+
+        private string generateScriptTags()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("<script type=\"text/javascript\">");
+            sb.AppendLine("{0}");
+            sb.AppendLine("{1}");
+            sb.AppendLine("</script>");
+            return sb.ToString();
+        }
+
+        private string getRedirectionUrl(string landingPage)
+        {
+            // strip the redirect URL out of the incomming URL
+            return string.Format("window.location = \"{0}\";", landingPage.Replace("/echo.html",""));
+        }
+
+        private string setLocalStorage(string headerKey, string headerValue)
+        {
+            return string.Format("localStorage.setItem(\'{0}\', \'{1}\');", headerKey, headerValue);
         }
     }
 }
