dc11f9f9c6d0a6e187a49bf19bf587914dca4f03
Theo Danzfuss <theodanzfuss@gmail.com>
2018-10-08 08:14:02 +0200

    Add debug interceptor

---
+++ RequestDebugInterceptor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Newtonsoft.Json.Linq;
+using System.Configuration;
+
+namespace Interceptor
+{
+    public class RequestDebugInterceptor : IHttpHandler
+    {
+        public bool IsReusable
+        {
+            get { return false; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            string headers = string.Empty;
+            foreach (string hKey in context.Request.Headers.AllKeys)
+            {
+                headers += hKey + " : " + context.Request.Headers.Get(hKey);
+                headers += "<br />";
+            }
+
+            context.Response.Clear();
+            context.Response.Write(headers);
+            context.Response.Flush();
+
+        }
+    }
+}
